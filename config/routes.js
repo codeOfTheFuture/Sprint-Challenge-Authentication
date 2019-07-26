327ec84ae941764ecee649b3b0824b6b63ad6b80
Jeff Oliver <jeffoliver3384@gmail.com>
2019-07-26 11:41:35 -0400

    created register and login functions and a function to generate a jwt for a user

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const { authenticate } = require('../auth/authenticate');
+const Users = require('./routes-model');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +13,41 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const user = req.body;
+
+  const hashPassword = bcrypt.hashSync(user.password, 14);
+
+  user.password = hashPassword;
+
+  Users.add(user)
+    .then(newUser => {
+      res.status(201).json({ id: newUser.id, username: newUser.username });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
   // implement user login
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `${user.username} is now logged in`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
@@ -29,4 +63,21 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
+}
+
+function generateToken(user) {
+  const jwtPayload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const jwtSecret =
+    process.env.JWT_SECRET ||
+    'add a .env file to root of project with the JWT_SECRET variable';
+
+  const jwtOptions = {
+    expiresIn: '1d',
+  };
+
+  return jwt.sign(jwtPayload, jwtSecret, jwtOptions);
 }
